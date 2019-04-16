dbe6f73c214d4f31767468a1e16ce423f10b8802
Richard Plant <repairist@gmail.com>
2019-04-16 13:22:38 +0100

    Fix DB url

--- ELG/settings.py
+++ ELG/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(env="JAWSDB_URL",conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
